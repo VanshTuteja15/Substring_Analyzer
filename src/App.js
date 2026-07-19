52d585ed2275f3afd9ed1bd50066d77adeae6293
Vansh <vansh.tuteja@edu.sait.ca>
2026-07-18 19:58:40 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <h1>
+        Longest Substring Analyzer 🚀
+      </h1>
+
+      <p>
+        LeetCode #3 - Longest Substring Without Repeating Characters
+      </p>
+
     </div>
+
   );
+
 }
+
 
 export default App;
